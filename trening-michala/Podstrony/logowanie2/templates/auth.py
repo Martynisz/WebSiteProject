a7c25a1b60337e1933e3acc05ef03225d94062e1
Michu_Malinowski <michumalinowski@gmail.com>
2023-01-16 22:33:14 +0100

    dodano obsługe formularza

--- trening-michala/Podstrony/logowanie2/templates/auth.py
+++ trening-michala/Podstrony/logowanie2/templates/auth.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Request, request
 
 auth=Blueprint("auth",__name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['get','post'])
 def login():
+    data=request.form
+    print(data)
     return render_template("login.html", text="testing",user="michu",bolean=True)
 @auth.route("/logout")
 def logout():
     return "<p>logout</p>"
-@auth.route("/sign_up")
+@auth.route("/sign_up", methods=['get','post'])
 def sign_up():
+    if request.method=="POST":
+        email=request.form.get("email")
+        firstname=request.form.get("name")
+        password1=request.form.get("password1")
+        password2=request.form.get("password2")
+        pass
+
+    
     return render_template("sign_up.html")
