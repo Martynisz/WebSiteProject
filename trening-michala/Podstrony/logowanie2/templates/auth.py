d5a862386c0a6f4b504f24c7f4783b15a1bd1ff3
Michu_Malinowski <michumalinowski@gmail.com>
2023-01-17 20:57:07 +0100

    Pasword and signup data check added, work on SQL started, SQLalchemy added to __init__.py

--- trening-michala/Podstrony/logowanie2/templates/auth.py
+++ trening-michala/Podstrony/logowanie2/templates/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, Request, request
+from flask import Blueprint, render_template, Request, request, flash
 
 auth=Blueprint("auth",__name__)
 
@@ -13,11 +13,39 @@ def logout():
 @auth.route("/sign_up", methods=['get','post'])
 def sign_up():
     if request.method=="POST":
+        
         email=request.form.get("email")
         firstname=request.form.get("name")
         password1=request.form.get("password1")
         password2=request.form.get("password2")
+        if check_email(email) & pass_check(password1,password2) & name_check(firstname):
+            flash("haslo,mail,nazwa prawidlowe --> konto utworzone",category="success")
         pass
 
     
     return render_template("sign_up.html")
+def check_email(email):
+    warun=True
+    return warun
+def name_check(name):
+    warun=True
+    if len(name)<3:
+        flash("your name must be at least 3 characters long",category="error")
+        warun=False
+    return warun
+def pass_check(password,password1):
+    znakispecjalne="!@#$%^&*()_+=?>.<;/"
+    warun=True
+    if len(password)<6:
+        flash("your password must be at least 6 characters long",category="error")
+        warun=False
+    if not any(i.isupper() for i in password ):
+        flash("your password must contain at least one UPPERCASE",category="error")
+        warun=False
+    if not any(i in znakispecjalne for i in password ):
+        flash("your password must contain at least one special character",category="error")
+        warun=False
+    if password!=password1:
+        flash("your passwords dont match",category="error")
+        warun=False
+    return warun
