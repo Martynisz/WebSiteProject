4d1f3a870373f9d2489aa0f0ab07fc021f8a7320
Michu_Malinowski <michumalinowski@gmail.com>
2023-01-19 21:55:54 +0100

    database initiation added with database structure

--- trening-michala/Podstrony/logowanie2/templates/__init__.py
+++ trening-michala/Podstrony/logowanie2/templates/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 db=SQLAlchemy()
 DB_NAME="database.db"
 def create_app(__name__):
@@ -12,4 +13,14 @@ def create_app(__name__):
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User, Note
+    with app.app_context():
+        db.create_all()
+        print("created database")    
     return app
+#create_database(app)
+#def create_database(app):
+    #if not path.exists("logowanie2/" + DB_NAME):
+        #db.create_all(app=app)
+        #print("created database")
