1c42321e8c5be46e2bff62c0eeb7b46f4f91930f
Michu_Malinowski <michumalinowski@gmail.com>
2023-01-21 18:06:23 +0100

    koniec pracy nad logowaniem do serwisu

--- trening-michala/Podstrony/logowanie2/templates/__init__.py
+++ trening-michala/Podstrony/logowanie2/templates/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 db=SQLAlchemy()
 DB_NAME="database.db"
 def create_app(__name__):
@@ -8,6 +9,7 @@ def create_app(__name__):
     app.config["SECRET_KEY"]="ASDASDSADASD"
     app.config["SQLALCHEMY_DATABASE_URI"]=f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
     from .views import views
     from .auth import auth
 
@@ -17,7 +19,14 @@ def create_app(__name__):
     from .models import User, Note
     with app.app_context():
         db.create_all()
-        print("created database")    
+        print("created database") 
+    login_manager=LoginManager()
+    login_manager.login_view="auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))   
     return app
 #create_database(app)
 #def create_database(app):
--- trening-michala/Podstrony/logowanie2/templates/auth.py
+++ trening-michala/Podstrony/logowanie2/templates/auth.py
@@ -1,15 +1,30 @@
-from flask import Blueprint, render_template, Request, request, flash
-
+from flask import Flask, Blueprint, render_template, Request, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 auth=Blueprint("auth",__name__)
 
 @auth.route("/login", methods=['get','post'])
 def login():
-    data=request.form
-    print(data)
+    if request.method=="POST":
+        email=request.form.get("email")
+        password=request.form.get("password")
+        if login_check(email, password):
+            user=User.query.filter_by(email=email).first()
+            login_user(user,remember=True)
+            return redirect(url_for("views.home"))
+
     return render_template("login.html", text="testing",user="michu",bolean=True)
+
+
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for("auth.login"))
+
+
 @auth.route("/sign_up", methods=['get','post'])
 def sign_up():
     if request.method=="POST":
@@ -19,13 +34,20 @@ def sign_up():
         password1=request.form.get("password1")
         password2=request.form.get("password2")
         if check_email(email) & pass_check(password1,password2) & name_check(firstname):
-            flash("haslo,mail,nazwa prawidlowe --> konto utworzone",category="success")
-        pass
+            create_new_user(email,password1,firstname)
+            login_user(user,remember=True)
+            return redirect(url_for('views.home'))
+        
 
     
     return render_template("sign_up.html")
+
 def check_email(email):
     warun=True
+    user=User.query.filter_by(email=email).first()
+    if user:
+        flash("this email exists on site already",category="error")
+        warun=False
     return warun
 def name_check(name):
     warun=True
@@ -39,13 +61,31 @@ def pass_check(password,password1):
     if len(password)<6:
         flash("your password must be at least 6 characters long",category="error")
         warun=False
-    if not any(i.isupper() for i in password ):
+    elif not any(i.isupper() for i in password ):
         flash("your password must contain at least one UPPERCASE",category="error")
         warun=False
-    if not any(i in znakispecjalne for i in password ):
+    elif not any(i in znakispecjalne for i in password ):
         flash("your password must contain at least one special character",category="error")
         warun=False
-    if password!=password1:
+    elif password!=password1:
         flash("your passwords dont match",category="error")
         warun=False
     return warun
+def create_new_user(email,password,firstname):
+    new_user=User(email=email,first_name=firstname,password=generate_password_hash(password,"sha256"))
+    db.session.add(new_user)
+    db.session.commit()
+    flash("haslo,mail,nazwa prawidlowe --> konto utworzone",category="success")
+    return True
+def login_check(email,password):
+    user=User.query.filter_by(email=email).first()
+    if user:
+        if check_password_hash(user.password,password):
+            flash("loged in succesfully", category="success")
+            return True
+        else:
+            flash("wrong password", category="error")
+            return False
+    else:
+        flash("no such email found", category="error")
+        return False
